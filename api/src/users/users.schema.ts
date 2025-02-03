ed59e869a286b381d81f29725c9f6e776a65dcd3
Claudius A. <90831377+ClaudiusAyadi@users.noreply.github.com>
2025-02-03 07:08:13 +0100

    Corrected the user relations.

--- api/src/users/users.schema.ts
+++ api/src/users/users.schema.ts
@@ -1,6 +1,7 @@
 import { pgTable, uuid, varchar, text } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 import { posts } from '../posts/posts.schema';
+import { tags } from '../tags/tags.schema';
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
@@ -9,7 +10,7 @@ export const users = pgTable('users', {
   role: varchar('role', { length: 20 }).notNull().default('reader'),
 });
 
-// Additional user details -  (one-to-one relations with users)
+// Additional user details -  (one-to-one with users)
 export const profile = pgTable('profile', {
   id: uuid('id').defaultRandom().primaryKey(),
   userId: uuid('user_id')
@@ -21,7 +22,20 @@ export const profile = pgTable('profile', {
   about: text('about'),
 });
 
-// A user can have many posts (i.e. be the writer for many posts) - one-to-many.
-export const usersRelations = relations(users, ({ many }) => ({
-  posts: many(posts),
+// A user can have just one profile but can write many posts/comments - one-to-many
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+  profile: one(profile, {
+    fields: [users.id],  
+    references: [profile.userId],  
+  }),
+  posts: many(posts), 
+  comments: many(comments),
 }));
+
+export const profileRelations = relations(profile, ({ one }) => ({
+  user: one(users, {
+    fields: [profile.userId],  
+    references: [users.id],  
+  }),
+})); 
